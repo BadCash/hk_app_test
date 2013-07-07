1564c5d5ed12bc9188e9ce411c0b49b58aff7a15
Magnus Wikhog <magnus@magnuswikhog.com>
2013-07-07 11:33:29 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+﻿// Wait for PhoneGap to load
+document.addEventListener("deviceready", onDeviceReady, false);
+
+$('[data-exec-function]').on('click', function(event){
+	console.log('exec_function');
+	window[ $(this).data('exec-function') ]($(this), event);
+});
+
+$('.item_title').on('click', function(event){
+	$(this).parent().parent().children().children('.item_content').hide();
+	$(this).siblings('.item_content').toggle();
+});
+
+$('.play_audio').on('click', function(event){
+	$(this).children('audio')[0].play();
+});
+
+
+// PhoneGap is ready
+function onDeviceReady() {
+	console.log("onDeviceReady");
+}
+
+
+function change_page(element, event){
+	console.log('change_page');
+	$('.page').hide();
+	$('.page[data-name="'+element.data('pagename')+'"]').show();
+}
